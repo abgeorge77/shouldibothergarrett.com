0c0c6280dff111e2ca4946d480517143bef6b224
abgeorge7 <abgeorge@umich.edu>
2019-10-24 16:18:44 -0500

    More features

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
+import answerQuestions from './js/answerQuestions';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          No.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      initialQuestion: '',
+      initialAnswer: '',
+    };
+  }
+
+  setInitialQuestion = e => {
+    this.setState({ initialQuestion: e.target.value });
+  }
+
+  askInitialQuestion = () => {
+    let answer = answerQuestions(this.state.initialQuestion);
+    this.setState({ initialAnswer: answer || 'What? '});
+  }
+
+  render() {
+    return (
+      <div className='app'>
+        <h1>Should I Bother Garrett?</h1>
+        <div className='app__initial-question-area'>
+          <textarea
+            className='app__textarea'
+            placeholder="Please type your question for Garrett here..."
+            value={this.state.initialQuestion}
+            onChange={this.setInitialQuestion}
+          />
+          <button className='app__button' onClick={() => this.askInitialQuestion()}>
+            Should I?
+          </button>
+          <h2>{this.state.initialAnswer}</h2>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
