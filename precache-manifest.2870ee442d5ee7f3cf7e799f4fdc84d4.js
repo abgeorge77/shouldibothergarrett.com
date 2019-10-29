cc4bb699de4e89cb20409bfd5c2667c0702b4751
abgeorge7 <abgeorge@umich.edu>
2019-10-29 10:59:25 -0500

    Updates

---
+++ precache-manifest.2870ee442d5ee7f3cf7e799f4fdc84d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48f74106493042df8a3861239a78731f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89620e72d0473c673e3b",
+    "url": "/static/css/main.e5f1d216.chunk.css"
+  },
+  {
+    "revision": "e49dfdba6d9c8e0cb7f4",
+    "url": "/static/js/2.c74df432.chunk.js"
+  },
+  {
+    "revision": "89620e72d0473c673e3b",
+    "url": "/static/js/main.b52a34d5.chunk.js"
+  },
+  {
+    "revision": "18589b90daec70062209",
+    "url": "/static/js/runtime~main.bf4025b5.js"
+  }
+]);
