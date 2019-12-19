d58ebaa298bcdb6bfcc3626a2fa9af73b62d741b
abgeorge77 <argeorge@comcast.net>
2019-12-19 15:24:23 -0600

    Updates

---
+++ precache-manifest.fbc4422c44271eac0bc207ea22f4fa44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "721cade495fa58730a32f796be76872c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f45db832f95aae49f172",
+    "url": "/static/css/main.e5f1d216.chunk.css"
+  },
+  {
+    "revision": "e49dfdba6d9c8e0cb7f4",
+    "url": "/static/js/2.c74df432.chunk.js"
+  },
+  {
+    "revision": "f45db832f95aae49f172",
+    "url": "/static/js/main.56925fda.chunk.js"
+  },
+  {
+    "revision": "18589b90daec70062209",
+    "url": "/static/js/runtime~main.bf4025b5.js"
+  }
+]);
